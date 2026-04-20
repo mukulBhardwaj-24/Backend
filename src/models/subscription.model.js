50fe5069ab0ad75aa42221e28264eacc547ade5e
mukulBhardwaj-24 <163962658+mukulBhardwaj-24@users.noreply.github.com>
2026-04-20 13:42:18 +0530

    added subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,36 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({}, {timestamps: true});
+const subscriptionSchema = new mongoose.Schema({
+    subscriber: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required : true
+    },
+
+    channel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required : true
+    },
+
+    validate : {
+        validator : function(channelId)
+        {
+            return channelId.toString() !== this.subscriber.toString();
+        },
+        message : "You cannot subscribe to yourself"
+    }
+}, { timestamps: true });
+
+subscriptionSchema.index(
+    {
+        subscriber: 1,
+        channel: 1
+    },
+    {
+        unique: true
+    }
+)
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
